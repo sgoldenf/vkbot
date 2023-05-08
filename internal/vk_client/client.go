44bdc1d785242304b4ccda0c662ee4bc26a4ce8f
sgoldenf <sgoldenf@student.21-school.ru>
2023-05-08 16:41:03 +0700

    feat: added second layer of callback buttons && bot navigation logic

--- internal/vk_client/client.go
+++ internal/vk_client/client.go
@@ -7,23 +7,23 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
+	"net/url"
 	"os"
 	"time"
 )
 
 const (
-	vkAPI                   string = "https://api.vk.com/method/"
-	apiVersion              string = "5.131"
-	getLongPollServerMethod string = "groups.getLongPollServer"
+	vkAPI      string = "https://api.vk.com/method/"
+	apiVersion string = "5.131"
 )
 
 type VkClient struct {
 	http.Client `json:"-"`
-	ApiToken    string      `json:"-"`
-	Session     sessionData `json:"response"`
-	errorLog    *log.Logger `json:"-"`
-	randomizer  *rand.Rand  `json:"-"`
-	params      *jsonParams `json:"-"`
+	ApiToken    string               `json:"-"`
+	Session     sessionData          `json:"response"`
+	errorLog    *log.Logger          `json:"-"`
+	randomizer  *rand.Rand           `json:"-"`
+	keyboards   map[string]*keyboard `json:"-"`
 }
 
 type sessionData struct {
@@ -31,10 +31,6 @@ type sessionData struct {
 	Key    string `json:"key"`
 	Ts     string `json:"ts"`
 	Wait   string `json:"-"`
-}
-
-type jsonParams struct {
-	firstLayerKeyboardJSON string
 }
 
 type pollData struct {
@@ -51,8 +47,12 @@ func New(accessToken, groupID string) *VkClient {
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 	s := rand.NewSource(time.Now().UnixNano())
 	r := rand.New(s)
-	vkClient := &VkClient{ApiToken: accessToken, errorLog: errorLog, randomizer: r}
-	vkClient.createJSONParams()
+	vkClient := &VkClient{
+		ApiToken:   accessToken,
+		errorLog:   errorLog,
+		randomizer: r,
+		keyboards:  newKeyboardMap(),
+	}
 	params := map[string]string{"group_id": groupID, "v": apiVersion}
 	resp, err := vkClient.get(vkAPI, getLongPollServerMethod, params)
 	if err != nil {
@@ -68,29 +68,6 @@ func New(accessToken, groupID string) *VkClient {
 	}
 	vkClient.Session.Wait = "25"
 	return vkClient
-}
-
-func (c *VkClient) createJSONParams() {
-	bytes, err := json.Marshal(firstLayerKeyboard)
-	if err != nil {
-		c.errorLog.Fatal(err)
-	}
-	c.params = &jsonParams{firstLayerKeyboardJSON: string(bytes)}
-}
-
-func (c *VkClient) get(urlPath, method string, params map[string]string) (*http.Response, error) {
-	urlPath += method
-	querySymbol := "?"
-	for k, v := range params {
-		urlPath += querySymbol + k + "=" + v
-		querySymbol = "&"
-	}
-	req, err := http.NewRequest("GET", urlPath, nil)
-	if err != nil {
-		return nil, err
-	}
-	req.Header.Set("Authorization", "Bearer "+c.ApiToken)
-	return c.Do(req)
 }
 
 func (c *VkClient) Poll() {
@@ -120,25 +97,84 @@ func (c *VkClient) Poll() {
 
 func (c *VkClient) processUpdate(u *update) {
 	if u.EventType == messageNew {
-		c.welcome(u.Object)
+		message := u.Object["message"].(map[string]interface{})
+		c.sendMessage(
+			uint64(message["from_id"].(float64)),
+			string(greetingMessage),
+			"main",
+		)
+	} else if u.EventType == messageEvent {
+		c.processMessageEvent(u.Object)
 	} else {
-		fmt.Println(u.EventType) // TODO: process message_event
+		c.errorLog.Println("event type not implemented: " + u.EventType)
 	}
 }
 
-func (c *VkClient) welcome(object map[string]interface{}) {
-	if message, ok := object["message"].(map[string]interface{}); ok {
-		userIdFloat := message["from_id"].(float64)
-		params := map[string]string{
-			"user_id":   fmt.Sprint(uint64(userIdFloat)),
-			"random_id": fmt.Sprint(c.randomizer.Int63()),
-			"message":   "Welcome!",
-			"keyboard":  c.params.firstLayerKeyboardJSON,
-			"v":         apiVersion,
+func (c *VkClient) processMessageEvent(object map[string]interface{}) {
+	c.sendMessageEventAnswer(object)
+	userID := uint64(object["user_id"].(float64))
+	payload := object["payload"].(map[string]interface{})
+	if button := buttonType(payload["button"].(string)); button == returnButton {
+		c.sendMessage(userID, string(greetingMessage), "main")
+	} else {
+		message := "You Chose Button " + payload["button"].(string) +
+			" in layer " + payload["layer"].(string)
+		var keyboard string
+		if k, ok := payload["keyboard"]; ok {
+			keyboard = k.(string)
 		}
-		_, err := c.get(vkAPI, "messages.send", params)
+		c.sendMessage(userID, message, keyboard)
+	}
+}
+
+func (c *VkClient) sendMessageEventAnswer(object map[string]interface{}) {
+	userIdFloat := object["user_id"].(float64)
+	userID := fmt.Sprint(uint64(userIdFloat))
+	peerID := object["peer_id"].(float64)
+	eventID := object["event_id"].(string)
+	params := map[string]string{
+		"event_id": eventID,
+		"user_id":  userID,
+		"peer_id":  fmt.Sprint(uint64(peerID)),
+	}
+	_, err := c.get(vkAPI, sendMessageEventAnswerMethod, params)
+	if err != nil {
+		c.errorLog.Println(err)
+	}
+}
+
+func (c *VkClient) sendMessage(userID uint64, message, keyboard string) {
+	params := map[string]string{
+		"user_id":   fmt.Sprint(userID),
+		"random_id": fmt.Sprint(c.randomizer.Int63()),
+		"message":   message,
+	}
+	if keyboard != "" {
+		keyboardBytes, err := c.keyboards[keyboard].toJSON()
 		if err != nil {
 			c.errorLog.Println(err)
+			return
 		}
+		params["keyboard"] = string(keyboardBytes)
 	}
+	_, err := c.get(vkAPI, messagesSendMethod, params)
+	if err != nil {
+		c.errorLog.Println(err)
+	}
+}
+
+func (c *VkClient) get(urlPath string, method method, params map[string]string) (*http.Response, error) {
+	urlPath += string(method)
+	queryParams := url.Values{}
+	for k, v := range params {
+		queryParams.Set(k, v)
+	}
+	queryParams.Set("v", apiVersion)
+	urlPath += "?" + queryParams.Encode()
+	req, err := http.NewRequest("GET", urlPath, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Authorization", "Bearer "+c.ApiToken)
+	return c.Do(req)
 }
